0474509429a80ac50e67b2812ed7dd9e9e559696
MARIAM MOUSTAPHA-AHMAT <moustapha-ahmat.mariam@etu.unistra.fr>
2025-04-29 12:59:14 +0200

    monprojet

--- contact.php
+++ contact.php
@@ -160,15 +160,6 @@
 			</section>
 		</section>
 	</section>
-	<section id="contact">
-		<h2>Contactez-moi</h2>
-		<form>
-			<input type="text" name="name" placeholder="Votre nom" required>
-			<input type="email" name="email" placeholder="Votre email" required>
-			<textarea name="message" placeholder="Votre message" required></textarea>
-			<button type="submit">Envoyer</button>
-		</form>
-	</section>
 
 </body>
 </html>
